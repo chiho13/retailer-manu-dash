1756934e2cce89ad1da80e86051161b380dadf02
Anthony Ho <ahomnky11@gmail.com>
2015-09-10 10:28:59 -0600

    first draft

--- admin-teammodmarket/assets/app.js
+++ admin-teammodmarket/assets/app.js
@@ -8,20 +8,20 @@ app.controller('Ctrl', function($scope) {
   $scope.user = {
     address: '10224 9th Street, Toronto, Ontario',
     email: "michael.balmer@freshco.ca",
-    status: 2
+    address2: '224 92nd Street, Chicago, Ontario',
+    email2: "gate.balmer@freshco.ca",
+
+    status: 2,
+    jdm: 1
   };  
 
-  // $scope.jdm = {
-  //   address: "23242 8th Street",
-  //   email: "gill.bates@jdm.ca"
-  //   status: 1
-  // }
 
   $scope.statuses = [
     {value: 1, text: 'Yes'},
     {value: 2, text: 'No'},
   
   ]; 
+
 
  
 });
